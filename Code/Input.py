0ce17a8d179a9f9e6a51fbb090b7c9a5ccecd20a
MatthewT <Firerexman007@gmial.com>
2020-04-28 13:40:07 -0500

    test input

---
+++ Code/Input.py
@@ -0,0 +1,95 @@
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import spidev
+import time
+import os
+
+# Open SPI bus
+spi = spidev.SpiDev()
+spi.open(0,0)
+
+DEBUG = False
+
+GPIO.setwarnings(False) # Ignore warning for now
+
+GPIO.setmode(GPIO.BCM) # Use the Broadcom pin mode
+
+buttons = [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 17, 16, 13]
+# what they mean L to R
+# right side:
+# 18, 19, 20, 21, 22, 23
+# Left side:
+# 24, 25, 26, 27, 12, 16
+# Extra:
+# 13 = ESC button
+
+GPIO.setup(buttons, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # set buttons to auto-down
+
+# Function to read SPI data from MCP3008 chip
+# Channel must be an integer 0-7
+def ReadChannel(channel):
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    data = ((adc[1] & 3) << 8) + adc[2]
+    return data
+
+
+# Define sensor channels
+# (channels 6 to 7 unused)
+swt_channel = 0
+vrx_channel = 1
+vry_channel = 2
+swt2_channel = 3
+vrx2_channel = 4
+vry2_channel = 5
+
+def rightJoystick():
+    # Read the first joystick position data
+    vrx_pos = ReadChannel(vrx_channel)
+    vry_pos = ReadChannel(vry_channel)
+    # Read switch state
+    swt_val = ReadChannel(swt_channel)
+    return vrx_pos, vry_pos, swt_val # Return x,y,and pressed postion
+
+def leftJoystick():
+    # Read the second joystick position data
+    vrx2_pos = ReadChannel(vrx_channel)
+    vry2_pos = ReadChannel(vry_channel)
+    # Read switch state
+    swt2_val = ReadChannel(swt_channel)
+    return vrx2_pos, vry2_pos, swt2_val  # Return x,y,and pressed postion
+
+def checkRight(button):
+    if (button == 18 and True):
+        print "Yellow"
+    if (button == 19 and True):
+        print "Gray"
+    if (button == 20 and True):
+        print "Green"
+    if (button == 21 and True):
+        print "Blue"
+    if (button == 22 and True):
+        print "Red"
+    if (button == 23 and True):
+        print "White"
+
+def checkLeft(button):
+    if (button == 24 and True):
+        pass
+    if (button == 25 and True):
+        pass
+    if (button == 26 and True):
+        pass
+    if (button == 27 and True):
+        pass
+    if (button == 12 and True):
+        pass
+    if (button == 16 and True):
+        pass
+
+while True:
+    for button in len(buttons):
+
+        if (button == 23 and True):
+            pass
+            # ESC
+        checkRight(button)
+        checkLeft(button)
